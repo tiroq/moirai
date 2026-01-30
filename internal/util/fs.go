a27528c4f1c0666117187dfbf1452c1bd52cccde
tiroq <isshamray@gmail.com>
2026-01-30 05:36:36 +0000

    Add Timestamp and CopyFileAtomic functions for file handling utilities

--- internal/util/fs.go
+++ internal/util/fs.go
@@ -1,9 +1,11 @@
 package util
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // ExpandUser expands a leading "~" to the current user's home directory.
@@ -31,4 +33,49 @@ func FileExists(path string) bool {
 // ListDir returns the directory entries for dir.
 func ListDir(dir string) ([]os.DirEntry, error) {
 	return os.ReadDir(dir)
+}
+
+// Timestamp returns a local time string formatted as YYYYMMDD-HHMMSS.
+func Timestamp() string {
+	return time.Now().Format("20060102-150405")
+}
+
+// CopyFileAtomic copies src to dst using a temp file in dst's directory.
+func CopyFileAtomic(src, dst string) error {
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dir := filepath.Dir(dst)
+	tempFile, err := os.CreateTemp(dir, ".tmp-")
+	if err != nil {
+		return err
+	}
+	tempName := tempFile.Name()
+	defer func() {
+		_ = os.Remove(tempName)
+	}()
+
+	if _, err := io.Copy(tempFile, srcFile); err != nil {
+		_ = tempFile.Close()
+		return err
+	}
+	if err := tempFile.Chmod(info.Mode().Perm()); err != nil {
+		_ = tempFile.Close()
+		return err
+	}
+	if err := tempFile.Close(); err != nil {
+		return err
+	}
+	if err := os.Rename(tempName, dst); err != nil {
+		return err
+	}
+	return nil
 }
